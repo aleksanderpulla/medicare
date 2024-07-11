a635110fbd7b6baab032f767acb1513c3a184dc6
Aleksandër Pulla <aleksanderpulla@gmail.com>
2024-07-12 01:18:45 +0200

    implement SMS notification

--- app/patients/[userId]/new-appointment/success/page.tsx
+++ app/patients/[userId]/new-appointment/success/page.tsx
@@ -5,18 +5,21 @@ import { formatDateTime } from "@/lib/utils";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import * as Sentry from "@sentry/nextjs";
+import { getUser } from "@/lib/actions/patient.actions";
 
 const Success = async ({
   params: { userId },
   searchParams,
 }: SearchParamProps) => {
   const appointmentId = (searchParams?.appointmentId as string) || "";
-
   const appointment = await getAppointment(appointmentId);
-
   const doctor = Doctors.find(
     (doc) => doc.name === appointment.primaryPhysician
   );
+  const user = await getUser(userId);
+
+  Sentry.metrics.set("user_view_appointment-success", user.name);
 
   return (
     <div className="flex h-screen max-h-screen px-[5%]">
--- lib/actions/appointment.actions.ts
+++ lib/actions/appointment.actions.ts
@@ -5,8 +5,9 @@ import {
   APPOINTMENT_COLLECTION_ID,
   DATABASE_ID,
   databases,
+  messaging,
 } from "../appwrite.config";
-import { parseStringify } from "../utils";
+import { formatDateTime, parseStringify } from "../utils";
 import { Appointment } from "@/types/appwrite.types";
 import { revalidatePath } from "next/cache";
 
@@ -106,10 +107,30 @@ export const updateAppointment = async ({
       throw new Error("Appointment not found");
     }
 
-    //TODO: SMS NOTIFICATION
+    const smsMessage = `
+    Hi, it's Medicare. 
+    ${type === "schedule" ? `Your appointment has been scheduled for ${formatDateTime(appointment.schedule!).dateTime}` : `We regret to inform you that your appointment has been canceled for the following reason: ${appointment.cancellationReason}`}
+    `;
+
+    await sendSMSNotification(userId, smsMessage);
 
     revalidatePath("/admin");
     return parseStringify(updatedAppointment);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const sendSMSNotification = async (userId: string, content: string) => {
+  try {
+    const message = await messaging.createSms(
+      ID.unique(),
+      content,
+      [],
+      [userId]
+    );
+
+    return parseStringify(message);
   } catch (error) {
     console.log(error);
   }
